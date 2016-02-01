da3b68f5ef94dddb76aaf86ec64393388fac65a9
Irina <i.malistova@yandex.ru>
2016-02-01 15:09:44 +0300

    change image directory

--- Prediction/src/ru/ks/prediction/Main.java
+++ Prediction/src/ru/ks/prediction/Main.java
@@ -30,7 +30,7 @@ public class Main {
 		
 		JButton btnNewButton = new JButton("");
 		btnNewButton.setBorder(null);
-		btnNewButton.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		btnNewButton.setIcon(new ImageIcon("Image//button.jpg"));
 		btnNewButton.setBounds(10, 10, 80, 80);
 		panel.add(btnNewButton);
 		btnNewButton.addActionListener(new ActionListener(){
@@ -44,7 +44,7 @@ public class Main {
 				
 		JButton button = new JButton("");
 		button.setBorder(null);
-		button.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button.setIcon(new ImageIcon("Image//button.jpg"));
 		button.setBounds(100, 10, 80, 80);
 		panel.add(button);
 		button.addActionListener(new ActionListener(){
@@ -58,7 +58,7 @@ public class Main {
 				
 		JButton button_1 = new JButton("");
 		button_1.setBorder(null);
-		button_1.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button_1.setIcon(new ImageIcon("Image//button.jpg"));
 		button_1.setBounds(190, 10, 80, 80);
 		panel.add(button_1);
 		button_1.addActionListener(new ActionListener(){
@@ -72,7 +72,7 @@ public class Main {
 				
 		JButton button_2 = new JButton("");
 		button_2.setBorder(null);
-		button_2.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button_2.setIcon(new ImageIcon("Image//button.jpg"));
 		button_2.setBounds(10, 100, 80, 80);
 		panel.add(button_2);
 		button_2.addActionListener(new ActionListener(){
@@ -86,7 +86,7 @@ public class Main {
 				
 		JButton button_3 = new JButton("");
 		button_3.setBorder(null);
-		button_3.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button_3.setIcon(new ImageIcon("Image//button.jpg"));
 		button_3.setBounds(100, 100, 80, 80);
 		panel.add(button_3);
 		button_3.addActionListener(new ActionListener(){
@@ -100,7 +100,7 @@ public class Main {
 				
 		JButton button_4 = new JButton("");
 		button_4.setBorder(null);
-		button_4.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button_4.setIcon(new ImageIcon("Image//button.jpg"));
 		button_4.setBounds(190, 100, 80, 80);
 		panel.add(button_4);
 		button_4.addActionListener(new ActionListener(){
@@ -114,7 +114,7 @@ public class Main {
 			
 		JButton button_5 = new JButton("");
 		button_5.setBorder(null);
-		button_5.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button_5.setIcon(new ImageIcon("Image//button.jpg"));
 		button_5.setBounds(10, 190, 80, 80);
 		panel.add(button_5);
 		button_5.addActionListener(new ActionListener(){
@@ -128,7 +128,7 @@ public class Main {
 			
 		JButton button_6 = new JButton("");
 		button_6.setBorder(null);
-		button_6.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button_6.setIcon(new ImageIcon("Image//button.jpg"));
 		button_6.setBounds(100, 190, 80, 80);
 		panel.add(button_6);
 		button_6.addActionListener(new ActionListener(){
@@ -142,7 +142,7 @@ public class Main {
 				
 		JButton button_7 = new JButton("");
 		button_7.setBorder(null);
-		button_7.setIcon(new ImageIcon("C:\\Users\\User\\workspace\\Prediction\\Image\\button.jpg"));
+		button_7.setIcon(new ImageIcon("Image//button.jpg"));
 		button_7.setBounds(190, 190, 80, 80);
 		panel.add(button_7);
 		button_7.addActionListener(new ActionListener(){
